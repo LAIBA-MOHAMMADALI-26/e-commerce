010398626a9d0c685f96a86fed792c97bbff2981
LAIBA <laibamohammadali@gmail.com>
2023-06-20 14:23:08 +0500

    category

---
+++ src/pages/Category.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navigation from '../components/Navigation'
+
+export default function Category() {
+  return (
+    <div>
+        <Navigation/>
+      category
+    </div>
+  )
+}
